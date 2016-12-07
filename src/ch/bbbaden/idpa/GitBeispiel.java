a80205217a3aefb2f40904445ce71558cb638ae6
Alexander Flick <Alexander Flick@WINDOWS-DLPVLEA>
2016-12-07 14:18:04 +0100

    Beispiel

---
+++ src/ch/bbbaden/idpa/GitBeispiel.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.bbbaden.idpa;
+
+/**
+ *
+ * @author Alexander Flick
+ */
+public class GitBeispiel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        HelloWorld hw = new HelloWorld("GIT");
+        System.out.println(hw.getAusgabe());
+    }
+    
+}
---
+++ src/ch/bbbaden/idpa/HelloWorld.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.bbbaden.idpa;
+
+/**
+ *
+ * @author Alexander Flick
+ */
+public class HelloWorld {
+    
+    private final String ausgabe;
+
+    public String getAusgabe() {
+        return ausgabe;
+    }
+
+    public HelloWorld(String ausgabe) {
+        this.ausgabe = ausgabe;
+    }
+
+}
